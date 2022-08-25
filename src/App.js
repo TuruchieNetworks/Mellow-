ea701446cde89ee118434182c97eeedbba5cd556
TuruchieNetworks <TuruchieNetworks@outlook.com>
2022-08-25 16:51:41 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { createFluxibleHook } from 'react-fluxible';
+import './App.css';
+import { createFluxibleHook } from 'react-fluxible';
+
+function mapStates({ authUser }) {
+  return { authUser };
+}
+
+function App() {
+  const { authUser } = createFluxibleHook(mapStates);
+  console.log('authUser', authUser);
+  
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route exact path='/Landing' element={ <Landing /> } />
+      <Route exact path='/Register' element={ <Register /> } />
+      <Route exact path='/Login' element={ <Login /> } />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+ 
